d728b05c133a364ba7eba145a171995490c80263
alexchrys99 <76920622+alexchrys99@users.noreply.github.com>
2025-03-13 22:42:42 +0100

    Add files via upload

---
+++ Nudenet_easylist_ngrok_filter_afterwards_v0.3/background.js
@@ -0,0 +1,14 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'predict') {
+        fetch('https://a0d3-178-51-62-246.ngrok-free.app/predict', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ base64_image: request.imageUrl })
+        })
+        .then((response) => response.json())
+        .then((data) => sendResponse(data))
+        .catch((error) => sendResponse({ error: error.message }));
+
+        return true; // Allows async response
+    }
+});
